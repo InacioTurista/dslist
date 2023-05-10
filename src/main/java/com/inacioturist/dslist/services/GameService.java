b880bb5f8feb8ad8969b6778857c4ef0eaa96216
InacioTurist <gabriel.inaciofn@gmail.com>
2023-05-10 20:26:56 -0300

    SQL consult, projection, search games by list

--- src/main/java/com/inacioturist/dslist/services/GameService.java
+++ src/main/java/com/inacioturist/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.inacioturist.dslist.dto.GameDTO;
 import com.inacioturist.dslist.dto.GameMinDTO;
 import com.inacioturist.dslist.entities.Game;
+import com.inacioturist.dslist.projections.GameMinProjection;
 import com.inacioturist.dslist.repositories.GameRepository;
 
 @Service
@@ -31,5 +32,11 @@ public class GameService {
 		// old way that was made
 		/* List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto; */
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
